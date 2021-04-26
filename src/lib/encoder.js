d4ceec3b40e2dbd11156b51dbdf40c907c80b580
Purwa Shrivastava <atq@SELUN-ATQ-MAC.local>
2021-04-26 14:45:24 +0200

    QB-4897 + QB-4916 + QB-4918 + QB-4990 February 2020 Patch 12.

---
+++ src/lib/encoder.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2010 - The OWASP Foundation
+ * <p/>
+ * The jquery-encoder is published by OWASP under the MIT license.
+ * <p/>
+ * This is encoder builds on jquery-encoder. It is refactored to suit the need for the QlikView Application.
+ * Since it no longer has any dependencies to jQuery it now called
+ * <em>encoder</em>
+ */
+ define([], function() {
+    var encoder = {
+      encodeForHTML: function (input) {
+        /** 
+        * Encodes input for use in HTML context
+        */    
+        if (input === undefined) {
+          return '';
+        }
+        let encoded = '',
+          encodingDiv = document.createElement('div');
+        const textNode = document.createTextNode(input);
+        encodingDiv.appendChild(textNode);
+        encoded = encodingDiv.innerHTML;
+        encodingDiv.removeChild(textNode);
+        return encoded;
+      },
+      htmlEscape: function(input) {
+        return input
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/\//g, '&#x2F;')   
+      }
+    };
+    return encoder;
+  })
